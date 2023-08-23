8c40d9a4c7b276b21770700b448bb529a121ea6d
Omarmed3rochsen123 <leo.almeblad@elev.ga.ntig.se>
2023-08-23 12:34:25 +0200

    Go agane kort

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,39 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner emil = new Scanner(System.in);
+        int goaganeonemoretime;
 
         int[] Kort = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
-        double index = Math.random() * Kort.length;
-        int first = Kort[(int) index];
-        System.out.println("Ditt kort är :" + first);
+        System.out.println("11 är knäckt, 12 dam, 13 kung, 14 ess");
+        System.out.print("Dra kort genom att skriva 1: ");
+        goaganeonemoretime = emil.nextInt();
 
-        index = Math.random() * Kort.length;
-        int second = Kort[(int) index];
-        System.out.println("Din motståndares kort är :" + second);
+        while (goaganeonemoretime == 1) {
 
-        if (first > second) {
-            System.out.println("Du vann!");
-        } else if (second > first) {
-            System.out.println("Du torskade lol");
-        } else {
-            System.out.println("Det blev lika, kör igen");
+            double index = Math.random() * Kort.length;
+            int first = Kort[(int) index];
+            System.out.println("Ditt kort är :" + first);
+
+            index = Math.random() * Kort.length;
+            int second = Kort[(int) index];
+            System.out.println("Din motståndares kort är :" + second);
+
+            if (first > second) {
+                System.out.println("Du vann!");
+            } else if (second > first) {
+                System.out.println("Du torskade lol");
+            } else {
+                System.out.println("Det blev lika, kör igen");
+            }
+
+            System.out.println("Skriv 1 för att go agane, något annat för att avsluta eller bara krascha programmet");
+            System.out.print(": ");
+            goaganeonemoretime = emil.nextInt();
+
         }
-
     }
 }
